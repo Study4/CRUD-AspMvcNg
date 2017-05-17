862f7489a3b159ef046f0ac08385851e9b96d7bf
Sky Chang <sancsky@livemail.tw>
2017-05-18 01:09:08 +0800

    Add Session Test

---
+++ Web/WebAppDemo.Web/Controllers/SesstionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppDemo.Web.Controllers
+{
+    public class SesstionController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Session["Test"] = "Hello Redis";
+            TempData["Test"] = "Hello Redis";
+
+            return View();
+        }
+
+        public ActionResult SessionTest()
+        {
+            return View();
+        }
+    }
+}
