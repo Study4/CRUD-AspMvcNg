8a38bd67fcb33f437dc9000d58f88671e2398516
Sky Chang <sancsky@livemail.tw>
2017-05-17 23:32:09 +0800

    Add Study4 Logo , Add Test API

---
+++ Api/WebAppDemo.Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAppDemo.Api.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
